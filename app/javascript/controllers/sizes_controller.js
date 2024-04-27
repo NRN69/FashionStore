d49bfe5a7e0d6ed9dd9898a3db85757c8544a031
NRN69 <HARRY-B@MAIL.RU>
2024-04-27 15:53:26 +0300

    Refactoring star bars code

--- app/javascript/controllers/sizes_controller.js
+++ app/javascript/controllers/sizes_controller.js
@@ -2,16 +2,16 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="sizes"
 export default class extends Controller {
-  changeclass() {
-    const elActivated = document.getElementById('activated');
+  changeClass() {
+    const elActivated = document.getElementById('activated')
 
-    this.element.dataset.option = 'true';
-    this.element.setAttribute('id', 'activated');
-    this.element.setAttribute('class', 'activated');
+    this.element.dataset.option = 'true'
+    this.element.setAttribute('id', 'activated')
+    this.element.setAttribute('class', 'activated')
 
     if (elActivated) {
-      elActivated.setAttribute('id', 'deactivated');
-      elActivated.setAttribute('class', '');
+      elActivated.setAttribute('id', 'deactivated')
+      elActivated.setAttribute('class', '')
     }
   }
 }
--- app/javascript/controllers/star_bar_controller.js
+++ app/javascript/controllers/star_bar_controller.js
@@ -3,38 +3,47 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="star-bar"
 export default class extends Controller {
   changeClass() {
-    const starActivatedValues = [];
-    const starActivated = document.getElementById('star-activated');
-    const starHiddenValue = document.getElementById('rating');
-    const starValues = document.querySelectorAll(
-      '[data-controller="star-bar"]',
-    );
+    
+    this.element.dataset.option = 'true'
 
-    this.element.dataset.option = 'true';
-    this.element.setAttribute('id', 'star-activated');
-    this.element.setAttribute('class', 'bi bi-star-fill');
-    starHiddenValue.setAttribute('value', this.element.getAttribute('value'));
-    // eslint-disable-next-line no-use-before-define
-    changeClassAnotherStar();
+    const currentStar = document.querySelector('[data-option="true"]')
+    const activated   = getElement('star-activated', 'id')
+    const hiddenValue = getElement('rating', 'id')
+    const stars       = getElement('[data-controller="star-bar"]')
+    
+    activated
+    ? Deactivated(activated,stars) 
+    : Activated(currentStar, hiddenValue, stars)
 
-    if (starActivated) {
-      starActivated.setAttribute('id', 'star-deactivated');
-      starActivated.setAttribute('class', 'bi bi-star');
-      starValues.forEach((el) => el.setAttribute('class', 'bi bi-star'));
+    function Activated(currentStar, hiddenValue, stars) {
+      currentStar.id =  'star-activated'
+      currentStar.setAttribute('class', 'bi bi-star-fill')
+      hiddenValue.setAttribute('value', currentStar.getAttribute('value'))
+      changeClassAnotherStar(stars)
     }
 
-    function changeClassAnotherStar() {
-      starValues.forEach((el) => {
-        if (
-          el.getAttribute('value') <
-          document.getElementById('star-activated').getAttribute('value')
-        ) {
-          starActivatedValues.push(el);
+    function Deactivated(activated,stars) {
+      activated.dataset.option = 'false'
+      activated.id = 'star-deactivated'
+      activated.setAttribute('class', 'bi bi-star')
+      stars.forEach(el => el.setAttribute('class', 'bi bi-star'))
+    }
+
+    function getElement(value, selector='') {
+      return selector.toLowerCase() === 'id'
+              ? document.getElementById(value)
+              : document.querySelectorAll(value)
+    }
+    
+    function changeClassAnotherStar(stars) {
+      let activeValue = hiddenValue.getAttribute('value')
+
+      stars.forEach(el => {
+        if (el.getAttribute('value') <= activeValue) { 
+
+          el.setAttribute('class', 'bi bi-star-fill')
         }
       });
-      starActivatedValues.forEach((el) =>
-        el.setAttribute('class', 'bi bi-star-fill'),
-      );
     }
   }
 }
